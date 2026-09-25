042dc6700080005421fc25eb5e334c979df2715b
Rohan Mukherjee <rohanmukherjee2007@gmail.com>
2026-09-25 18:26:08 +0530

    Fixed index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 import path from "path";
 import { fileURLToPath } from "url";
 import Listing from './models/listing.js';
-import { rmSync } from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,6 +11,7 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
